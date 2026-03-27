c536b21d6328c2b1dd7b149bf57c81ff4b942abc
SrijanChitranshi <srijanchitranshi1@gmail.com>
2026-03-27 18:43:23 +0530

    Add DiagonalSum class with main method

---
+++ DiagonalSum.java
@@ -0,0 +1,16 @@
+public class DiagonalSum {
+
+    public static void diagonal_Sum(int[][]matrix){
+
+    }
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 16}
+        };
+
+        diagonal_Sum(matrix);
+    }
+}
